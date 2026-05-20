1a5ba52afc00f7c7571724b587720a2a6cdaf26a
walter-zsl <345154004@qq.com>
2026-05-20 17:21:40 +0800

    init

---
+++ store-cloud-sample/src/main/java/com/store/cloud/sample/HelloController.java
@@ -0,0 +1,13 @@
+package com.store.cloud.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "store-cloud-sample ok";
+    }
+}
