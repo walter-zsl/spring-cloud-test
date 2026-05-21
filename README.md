bdce09fa23da0656df8a7ca932ca4398693e9e80
walter-zsl <345154004@qq.com>
2026-05-21 09:31:08 +0800

    add read apollo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spring-cloud-test
 本地实践一下spring colud搭建
+
+## 增加apollo配置
+### http://localhost:8070/config.html?#/appid=store-sample demo.msg = Hello from Apollo
--- store-cloud-sample/src/main/java/com/store/cloud/sample/HelloController.java
+++ store-cloud-sample/src/main/java/com/store/cloud/sample/HelloController.java
@@ -1,13 +1,19 @@
 package com.store.cloud.sample;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 public class HelloController {
+    private final String demoMsg;
+
+    public HelloController(@Value("${demo.msg:}") String demoMsg) {
+        this.demoMsg = demoMsg;
+    }
 
     @GetMapping("/hello")
     public String hello() {
-        return "store-cloud-sample ok";
+        String msg = demoMsg.isBlank() ? "demo.msg 未就绪（Apollo 未连上或未发布键）" : demoMsg;
+        return "store-cloud-sample ok, demo.msg=" + msg;
     }
 }
